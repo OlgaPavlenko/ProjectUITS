d21543e33b535579de5e02a08929522c764eba62
OlgaPavlenko <olgaigpavlenko@gmail.com>
2017-05-05 10:58:09 +0300

    accordeon

--- accordeonModule.js
+++ accordeonModule.js
@@ -4,18 +4,24 @@ var accordeonModule = function (data) {
 
 	this.data = {id: 2};
 	
-	var elem = document.querySelector('.content');
+	$h.d('.accordion-module', 'click', '.item', function (e) {
 
-		elem.addEventListener('click', function (e) {
+			var open = document.querySelectorAll('.box:not(.collapsed-box)');
 
-			if (e.target.classList.contains('fa-plus') ) {
 
-					var openElem = e.target.closest('.box-default');
-						openElem.classList.remove('collapsed-box');
-			}
+			for (var i = 0; i < open.length; i++ ) {
 
-		});
-}
+				open[i].classList.add('collapsed-box');
+	 		}
+
+	 		var openElem = this.closest('.box-default');
+			openElem.classList.remove('collapsed-box');
+	})
+
+	
+
+		};
+
 
 accordeonModule.prototype = Object.create(App.prototype);
 accordeonModule.constructor = accordeonModule;
--- helper.js
+++ helper.js
@@ -15,11 +15,42 @@
 		if (elem == null) {
 
 			console.warn('No elements found!');
+
 			return;
-		}
+		};
 
 		elem.addEventListener(eventType, callback);
 	}
+
+	$h.d = function (parent, type, child, callback) {
+
+	 	parent = document.querySelector(parent);
+
+		if (parent == null) {
+
+			console.warn('No elements found!');
+
+			return;
+		}
+
+		parent.addEventListener(type, function(e) {
+
+			var target = e.target;
+
+			while(target != parent) {
+
+				if(target.matches(child)) {
+
+					callback.call(target, e);
+					return;
+				}
+
+				target = target.parentNode;
+			}
+				
+		});
+	}
+	
 
 		window.$h = $h;
 		
